b8c91ce3512cc25d1ec62f08630d845908b1ee59
HareshTrivedi <tnisarghu90@gmail.com>
2019-04-23 21:02:40 +0530

    c	new file:   books/admin.py c	new file:   books/apps.py c	new file:   books/models.py c	new file:   books/tests.py c	modified:   learn/templates/learn/base.html

---
+++ books/models.py
@@ -0,0 +1,65 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+from django.db.models import Q
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class BooksQuerySet(models.QuerySet):
+    def published(self):
+        now = timezone.now()
+        return self.filter(publish_date__lte=now)
+
+    def search(self, query):
+        lookup = (
+                    Q(title__icontains=query) |
+                    Q(content__icontains=query) |
+                    Q(slug__icontains=query) |
+                    Q(user__first_name__icontains=query) |
+                    Q(user__last_name__icontains=query) |
+                    Q(user__username__icontains=query)
+                    )
+
+        return self.filter(lookup)
+
+
+class BookManager(models.Manager):
+    def get_queryset(self):
+        return BooksQuerySet(self.model, using=self._db)
+
+    def published(self):
+        return self.get_queryset().published()
+
+    def search(self, query=None):
+        if query is None:
+            return self.get_queryset().none()
+        return self.get_queryset().published().search(query)
+
+
+class Books(models.Model):
+    # id = models.IntegerField() # pk
+    title = models.TextField()
+    chapter = models.TextField()
+    sub_chapter = models.TextField()
+    topics = models.TextField()
+    content = models.TextField()
+    publish_date = models.DateTimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL)
+    image = models.ImageField(upload_to='image/', blank=True, null=True)
+
+    objects = BookManager()
+
+    class Meta:
+        ordering = ['-publish_date', '-updated', '-timestamp']
+
+    def get_absolute_url(self):
+        return f"/blog/{self.slug}"
+
+    def get_edit_url(self):
+        return f"{self.get_absolute_url()}/edit"
+
+    def get_delete_url(self):
+        return f"{self.get_absolute_url()}/delete"
